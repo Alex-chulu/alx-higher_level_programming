d379fe55cadae5386bd754c35b3fe1e3ba4563c0
Alex-chulu <chulualexandar@gmail.com>
2022-12-06 16:17:59 -0800

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cp = my_list.cp()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.cp
+    else:
+        cp[idx] = element
+        return cp
