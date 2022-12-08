484a256bbc8a094767dc63a1e8b3cefaeb3d78da
Alex-chulu <chulualexandar@gmail.com>
2022-12-07 17:33:17 -0800

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.cp()
+
+    for x in range(len(matrix)):
+        new_matrix[x] = list(map(lambda y: y**2, matrix[x]))
+    return (new_matrix)
