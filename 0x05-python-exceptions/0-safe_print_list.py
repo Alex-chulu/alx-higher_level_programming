d96368b38ac2392e31fb7e2dd700d5193eb57121
Alex-chulu <chulualexandar@gmail.com>
2022-12-20 17:59:50 +0200

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,9 @@ def safe_print_list(my_list=[], x=0):
     rmp = 0
     for y in range(x):
         try:
-            print("{}".format(my_list[y], end=""))
+            print("{}".format(my_list[y]), end="")
             rmp += 1
         except IndexError:
             break
+    print("")
     return (rmp)
