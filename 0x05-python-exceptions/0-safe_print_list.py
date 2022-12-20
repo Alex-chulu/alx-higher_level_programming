a66c5c8280a6b1c8563a32a6c8a4aa41d57f8d7f
Alex-chulu <chulualexandar@gmail.com>
2022-12-20 07:17:45 -0800

    task 0 fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-
+    """
     Args:
         my_list: list of elements.
         x: number of elements to print.
     Return: number of elements
-
+    """
     rmp = 0
     for y in range(x):
         try:
