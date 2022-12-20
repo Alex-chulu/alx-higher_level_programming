fbd0c674a55d55ff73b4a6f1e79be5dac1143b0f
Alex-chulu <chulualexandar@gmail.com>
2022-12-20 16:49:47 +0200

    three tasks

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    Args:
+        my_list: list of elements.
+        x: number of elements to print.
+    Return: number of elements
+
+    rmp = 0
+    for y in range(x):
+        try:
+            print("{}\n".format(my_list[y], end=""))
+            rmp += 1
+        except IndexError:
+            break
+    return (rmp)
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+
+    Args:
+        value: int to be printed
+        Return: True if no error occured else Fals
+    try:
+        print({":d}".format(value))
+        return (True)
+    except (TypeError, ValueError)
+        return (False)
