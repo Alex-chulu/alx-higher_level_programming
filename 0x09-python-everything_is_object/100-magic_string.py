132c75ce8cbe090bf047b9f07dfdefd86b49e596
Alex-chulu <chulualexandar@gmail.com>
2023-01-09 07:38:46 +0200

    magic_string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,6 @@
-for i in range(10):
-    for j in range(0, i + 1):
-        print("BestSchool ", end="")
-    print()
+def magic_string(n):
+    for i in range(0, n):
+        for j in range(0, i+1):
+            print("BestSchool ", end="")
+        print("\r")
+magic_string(10)
