99282510f833c8493d52f0c75bf1264d9d6e9ab2
Alex-chulu <chulualexandar@gmail.com>
2023-01-09 08:25:37 +0200

    task 19 fix

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def copy_list(l):
-    fruits = ['apple', 'banana', 'cherry', 'orange']
-    print (fruits.copy())
+def copy_list(l, fruits=['apple', 'banana', 'cherry', 'orange']):
+    print(fruits.copy())
+copy_list(1)
