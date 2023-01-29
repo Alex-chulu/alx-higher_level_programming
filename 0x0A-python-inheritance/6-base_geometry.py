8396e4e93a93a5b2ebce4ec7a8bed0d448650950
Alex-chulu <chulualexandar@gmail.com>
2023-01-29 17:44:25 +0200

    6 fix

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""This is class BaseGeometry (based on 5-base_geometry.py)"""
+
 
 class BaseGeometry:
+    """
+    public method: that raises an Exception with
+    the message area() is not implemented
+    """
     def area(self):
         raise Exception("area() is not implemented")
-
-obj = BaseGeometry()
