35818a4b8a55864604919355b977ce8d7955f498
Alex-chulu <chulualexandar@gmail.com>
2023-01-29 16:41:41 +0200

    lookup file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""returns the list ofavailable attributes
+    and methods of an object
+    """
+
+
+def lookup(obj):
+    """
+    The 'dir()' function will return a list of available attributes and
+    methods of an object.
+
+    Args:
+        obj (list): object of the class object
+    Returns:
+        list: list of object
+    """
+    return dir(obj)
+
