c5044dd2ad6e0c2054dfcf8e9407b74274b3f164
Alex-chulu <chulualexandar@gmail.com>
2023-01-30 17:12:57 +0200

    five test file

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Testing for max_integer"""
+
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class MaxIntTest(unittest.TestCase):
+
+    def max_int_test(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([-5, -9, 0, -4]), 0)
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_boolean(self):
+        self.assertTrue(max_integer([True, False]), True)
+
+    def max_string_test(self):
+        self.assertEqual(max_integer(["hellow world"]), "hello world")
