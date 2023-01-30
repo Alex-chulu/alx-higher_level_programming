1492ab8ab3bd1b3111bea171c9ecc60f17715297
Alex-chulu <chulualexandar@gmail.com>
2023-01-30 16:52:36 +0200

    task 3 fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,12 @@
-3-say_my_name.py
 #!/usr/bin/python3
 """Defines a function that prints a name."""
 
 
 def say_my_name(first_name, last_name=""):
     """Print out name.
-
     Args:
         first_name (str): First name to be printed.
         last_name (str): Last name to be printed.
-
     Raise:
         TypeError: First_name or last_name must be a strings.
     """
