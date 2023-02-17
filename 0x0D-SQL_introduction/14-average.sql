36b9bf23aecbe62c13680df1af05e4517ba752e4
Alex-chulu <chulualexandar@gmail.com>
2023-02-17 08:47:32 -0800

    task 14 fix

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- a script that computes the score average of all records in 
 -- the table second_table of the database hbtn_0c_0
-SELECT ROUND(AVG(score)) FROM second_table AS average;
+SELECT AVG(score) FROM second_table AS average;
