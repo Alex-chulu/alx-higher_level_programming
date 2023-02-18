d140060ea8f4dde836185b4b0bce283315162221
Alex-chulu <chulualexandar@gmail.com>
2023-02-17 18:26:32 -0800

    No Comedy tonight!

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,5 @@
+-- a script that lists all shows without the genre Comedy 
+-- in the database hbtn_0d_tvshows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id ORDER BY rating DESC;
