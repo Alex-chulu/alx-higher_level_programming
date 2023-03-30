8f0bb5126a7d735a46079b203debab894633f704
Alex-chulu <chulualexandar@gmail.com>
2023-03-29 18:26:00 -0700

    task 9 fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,7 +6,7 @@ const num_2 = process.argv[3];
 if (isNaN(num_1) || isNaN(num_2)) {
   console.log('NaN');
 } else {
-  console.log(addtn(parseInt(num_1), parseInt(num_2)));
+  console.log(add(parseInt(num_1), parseInt(num_2)));
 }
 
 function add(a, b) {
