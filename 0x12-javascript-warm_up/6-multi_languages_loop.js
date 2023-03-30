cf8cd28e8da05d07241fc328c61c56a922210640
Alex-chulu <chulualexandar@gmail.com>
2023-03-29 18:05:10 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const multi_language = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let x = 0; x < multi_language.length; x++) {
+  console.log(multi_language[x]);
+}
