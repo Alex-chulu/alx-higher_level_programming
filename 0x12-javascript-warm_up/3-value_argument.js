78ad41a3910f579eb7b2afd94f0c584f59829b79
Alex-chulu <chulualexandar@gmail.com>
2023-03-29 17:47:23 -0700

    value args

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
