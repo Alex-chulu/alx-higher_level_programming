a5a8fe065f6d6cb2d860c0e7a8a470aebbc2fd62
Alex-chulu <chulualexandar@gmail.com>
2023-03-29 17:42:10 -0700

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args.length === 0){
-	console.log('No argument');
-}
-else if (args.length === 1){
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
