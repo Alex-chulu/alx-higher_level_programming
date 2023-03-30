ddc2bc37ba48a5d30fc869455aefea3aef007b11
Alex-chulu <chulualexandar@gmail.com>
2023-03-30 15:26:29 -0700

    task 0 fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-curl --head "$1" | grep 'Content-Length' | cut -d " " -f 2
+curl -sL "$1"
