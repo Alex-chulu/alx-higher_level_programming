edbd1c66253ec0d54926dbaac5264a317d304370
Alex-chulu <chulualexandar@gmail.com>
2023-03-31 02:31:46 -0700

    task 7 fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""a script that lists all State objects from the database hbtn_0e_6_usa
 """
-from sqlalchemy import (create_engine)
-from model_state import Base, State
+script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
-
-def allstates():
-    """list all states from the database
+if __name__ == "__main__":
     """
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                sys.argv[1],
-                sys.argv[2],
-                sys.argv[3]),
-            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    Maka a database connection and get the states
+    from the database.
+    """
 
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-
-if __name__ == '__main__':
-    allstates()
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
