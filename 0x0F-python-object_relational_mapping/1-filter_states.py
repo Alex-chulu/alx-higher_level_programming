b83241b05a6197361ae2156e41d3500ec19554e6
Alex-chulu <chulualexandar@gmail.com>
2023-03-31 01:29:50 -0700

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+
+def filter():
+    """Make a databse connection"""
+    db_conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db_conn=argv[3])
+    cur = db_conn.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == 'N':
+            print(row)
+
+    db_conn.close()
+
+if __name__ == '__main__':
+    filter()
