b5e9e3568bfac1bc6c5a3920f444449b39492348
Alex-chulu <chulualexandar@gmail.com>
2023-04-04 00:58:00 -0700

    task 0 fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = reponse.read()
+    html = response.read()
     print('Body response:')
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
-    print('\- utf8 content: {}'.format(html.decode('utf8')))
+    print('\t- utf8 content: {}'.format(html.decode('utf8')))
