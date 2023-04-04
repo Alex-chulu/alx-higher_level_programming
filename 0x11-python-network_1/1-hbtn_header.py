51feb10f2cc750e9b8a90fb88f4a8e731b2d5727
Alex-chulu <chulualexandar@gmail.com>
2023-04-04 01:24:00 -0700

    task 1 fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,8 @@ value of the X-Request-Id variable found in the header of the response
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
