6bbdb9474d44c584b5eb5316f8f12c89c0027f3d
Alex-chulu <chulualexandar@gmail.com>
2023-04-04 04:19:17 -0700

    task 7 fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     response = requests.get(url)
 
     if response.status_code >= 400:
-        print("Error code:", response.status_code)
+        print("Error code: {}".format(response.status_code))
     else:
-        print(response.content)
+        print(response.text)
