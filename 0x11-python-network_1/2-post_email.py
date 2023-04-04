5ac65fe6d3f5de52fa09f31eaa39cfde91533d5b
Alex-chulu <chulualexandar@gmail.com>
2023-04-04 02:00:13 -0700

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email})
+data = data.encode('utf-8')#data should be bytes
+
+with urllib.request.urlopen(url, data) as response:
+    html = response.read()
+    print(html.decode('utf-8'))
