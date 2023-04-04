d0202b4fa7b039955dbd792242825e8a92d0627d
Alex-chulu <chulualexandar@gmail.com>
2023-04-04 02:43:06 -0700

    task 2 fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+Python script that takes in a URL and an email, sends a POST request 
+to the passed URL with the email as a parameter
+"""
 import sys
 import urllib.parse
 import urllib.request
