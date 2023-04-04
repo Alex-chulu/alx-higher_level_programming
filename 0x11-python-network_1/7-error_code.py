4d4408e5614036ea6ebe4557c77d295ebcb652a2
Alex-chulu <chulualexandar@gmail.com>
2023-04-04 04:15:20 -0700

    task 7 fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,8 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    url = input("Enter a URL: ")
+    url = sys.argv[1]
+
     response = requests.get(url)
 
     if response.status_code >= 400:
