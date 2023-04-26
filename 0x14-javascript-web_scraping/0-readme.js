4425012f05b6a1a11c76738cdbb5208d911a3403
Alex-Chulu <chulualexandar@gmail.com>
2023-04-26 02:28:03 +0000

    task 0 fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // Include fs module
-const fs = require("fs");
+const fs = require('fs');
 
 if (process.argv.length !== 3) {
-  console.log("Usage: node cisfun");
+  console.log('Usage: cisfun');
   process.exit(1);
 }
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
   if (err) {
     console.error(err);
   } else {
